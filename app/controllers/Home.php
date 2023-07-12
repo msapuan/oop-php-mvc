3134b594fb1a4bea6879a31d7130e412d9dd5d79
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2023-07-12 10:09:48 +0700

    Update Home.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,6 +4,8 @@
     // buat method DEFAULT
     public function index()
     {
+      $this->view('templates/header');
       $this->view('home/index');
+      $this->view('templates/footer');
     }
   }
