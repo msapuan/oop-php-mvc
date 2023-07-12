c4728ea54aaeed43187c9a31eaccbc8d06dc7d1e
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2023-07-12 11:42:48 +0700

    update

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,9 @@
+<div class="container">
 
-  <h1>About me</h1>
+  <h1 class="mt-4">About me</h1>
+  <img src="<?= BASEURL; ?>/img/coder-profile.jpg" alt="Coder" width="200" class="rounded-circle shadow">
   <p>
     Saya adalah <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>. Saya seorang <?= $data['pekerjaan']; ?>
   </p>
+
+</div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,5 @@
-<script src="https://localhost/oop-php-mvc/public/js/jquery.slim.min.js"></script>
-<script src="https://localhost/oop-php-mvc/public/js/popper.min.js"></script>
-<script src="https://localhost/oop-php-mvc/public/js/bootstrap.js"></script>
+<script src="<?= BASEURL; ?>/js/jquery.slim.min.js"></script>
+<script src="<?= BASEURL; ?>/js/popper.min.js"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
 </body>
 </html>
