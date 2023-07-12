84134864031397207bc08788c32fbeaeaf8fde6d
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2023-07-12 10:12:26 +0700

    Update About.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -7,12 +7,16 @@
       $data['pekerjaan'] = $pekerjaan;
       $data['umur'] = $umur;
       
+      $this->view('templates/header');
       $this->view('about/index', $data);
+      $this->view('templates/footer');
     }
 
     public function page()
     {
+      $this->view('templates/header');
       $this->view('about/page');
+      $this->view('templates/footer');
     }
 
   }
